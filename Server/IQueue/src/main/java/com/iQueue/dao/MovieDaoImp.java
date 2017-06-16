18449218377d4cb4422cf3bdcf2d19eb9e852b75
David-Tan-YiFan <704778682@qq.com>
2017-06-16 15:58:55 +0800

    Revert "Upload Packages"

--- Server/IQueue/src/main/java/com/iQueue/dao/MovieDaoImp.java
+++
@@ -1,9 +0,0 @@
-package com.iQueue.dao;
-
-import java.util.List;
-import com.iQueue.model.Movie;
-
-public interface MovieDaoImp {
-	public void insert(Movie movie);
-	public Movie getMovie(String movieName);
-}
--- Server/IQueue/src/main/java/com/iQueue/dao/UserDaoImp.java
+++
@@ -1,9 +0,0 @@
-package com.iQueue.dao;
-
-import java.util.List;
-import com.iQueue.model.User;
-
-public interface UserDaoImp {
-	public void insert(User user);
-	public User getUser(String userName);
-}
